8a26738d86e5c4f67f91e3b8a53386ab9f1216fe
Rohan121002 <f20212739@hyderabad.bits-pilani.ac.in>
2024-04-21 11:31:18 +0530

    exp 2 almost done

--- exp2.py
+++ exp2.py
@@ -11,8 +11,8 @@ punctuations = ":(){}[],.';/"
 
 
 def read_documents_from_directory(directory):
-    documents = defaultdict(str)
-    titles = defaultdict(str)
+    documents = {}
+    titles = {}
     count = 0
     for filename in os.listdir(directory):
         if filename.endswith("doc_dump.txt"):
@@ -24,16 +24,30 @@ def read_documents_from_directory(directory):
                     parts = line.strip().split('\t')
                     if (parts[0] == 'MED-5371'):
                         doc_num, link, title, content = parts[0], parts[1], parts[2], parts[3]
-                        documents[doc_num] += content
-                        titles[doc_num] += title
+                        documents[doc_num] = content
+                        titles[doc_num] = title
                     else:
                         doc_num, link, title, content = parts[0], parts[1], parts[2], parts[3]
-                        documents[doc_num] += content
-                        titles[doc_num] += title
+                        documents[doc_num] = content
+                        titles[doc_num] = title
 
     return documents, titles, count
 
 # Function to tokenize documents into terms
+
+
+def getDocFreq():
+    df = {}
+    with open('nfcorpus/index/postings.tsv', 'r', encoding='utf-8') as file:
+        for l in file:
+            line = l.split('\t')
+            temp = line[0]
+            for x in temp:
+                if x in punctuations:
+                    temp = temp.replace(x, '')
+            if len(temp) >= 1:
+                df[temp] = line[2]
+    return df
 
 
 def tokenize_document(document):
@@ -45,14 +59,19 @@ def tokenize_document(document):
 def calculate_term_frequencies(documents):
     term_frequencies = {}
     for doc_name, content in documents.items():
-        terms = defaultdict(int)
+        terms = {}
         tokens = tokenize_document(content)
         for token in tokens:
-            if token not in punctuations:
-                terms[token] += 1
+            for x in token:
+                if x in punctuations:
+                    token = token.replace(x, '')
+            if len(token) >= 1:
+                if token not in terms:
+                    terms[token] = 1
+                else:
+                    terms[token] += 1
         term_frequencies[doc_name] = terms
     return term_frequencies
-# Function to calculate Euclidean length of a vector
 
 
 def norm(terms):
@@ -64,7 +83,6 @@ def norm(terms):
 def normalize_c(term_frequencies):
     normalized_documents = {}
     for doc_name, terms in term_frequencies.items():
-        # print("Hemlo", terms)
         norm_length = norm(terms)
         normalized_terms = {term: freq /
                             norm_length for term, freq in terms.items()}
@@ -72,14 +90,18 @@ def normalize_c(term_frequencies):
     return normalized_documents
 
 
-def normalize_ntn(term_frequencies, total_docs):
-    with open('nfcorpus/raw/doc_dump.txt', 'r', encoding='utf-8') as file:
+def normalize_ntn(total_docs):
+    t_df = {}
+    with open('nfcorpus/index/postings.tsv', 'r', encoding='utf-8') as file:
         for l in file:
             line = l.split('\t')
-            for x in line[0]:
+            temp = line[0]
+            for x in temp:
                 if x in punctuations:
-                    x.replace('')
-            df[line[0]] = line[2]/total_docs
+                    temp = temp.replace(x, '')
+            if len(temp) >= 1:
+                t_df[temp] = math.log10(total_docs/int(line[2]))
+    return t_df
 
 
 # Directory containing .txt files
@@ -87,13 +109,17 @@ directory = "./nfcorpus/raw"
 
 # Read documents from .txt files in the specified directory
 documents, titles, total_docs = read_documents_from_directory(directory)
-df = {}
+df = getDocFreq()
 
 # Calculate term frequencies in documents
 term_frequencies_docs = calculate_term_frequencies(documents)
 term_frequencies_title = calculate_term_frequencies(titles)
 print("NNN")
 print()
+for term, doc_freq in df.items():
+    print(f"term '{term} has document frequency as '{doc_freq}'")
+    break
+
 for doc_name, normalized_terms in term_frequencies_docs.items():
     print(f"Document '{doc_name}' has this normalized vector:",
           normalized_terms)
@@ -105,16 +131,15 @@ for doc_name, normalized_terms in term_frequencies_title.items():
     break
 
 print("NTN")
-print()
-normalized_ntn = normalize_ntn(term_frequencies_docs)
+print("Only the Document frequency changes for this:")
+t_df = normalize_ntn(total_docs)
+for term, doc_freq in t_df.items():
+    print(f"term '{term} has document frequency as '{doc_freq}'")
+    break
 
-# normalized_nnn = normalize_c(term_frequencies_docs)
-# for doc_name, normalized_terms in normalized_nnn.items():
-#     print(f"Document '{doc_name}' has this normalized vector:",
-#           normalized_terms)
-#     break
-
-# for doc_name, normalized_terms in term_frequencies_title.items():
-#     print(f"Title '{doc_name}' has this normalized vector:",
-#           normalized_terms)
-#     break
+print("NTC")
+print("Only the vector will be cosine normalize")
+normalized_ntc = normalize_c(term_frequencies_docs)
+for doc_term, normalized_ter in normalized_ntc:
+    print(f"'{doc_term} has normalized vector as '{normalized_ter}'")
+    break
