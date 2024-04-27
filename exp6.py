e73264e7fb03c232636ad72497b0c4729812fe4c
Rohan121002 <f20212739@hyderabad.bits-pilani.ac.in>
2024-04-27 23:42:57 +0530

    exp6 done

--- exp6.py
+++ exp6.py
@@ -4,6 +4,7 @@ import csv
 from nltk import FreqDist
 from nltk import word_tokenize
 import re
+import exp3Rocchio
 
 
 def getAllEntity():
@@ -16,9 +17,9 @@ def getAllEntity():
             entityList.append(row[0])
             if row[2] != "No synonyms":
                 if row[0] not in entityRelation.keys():
-                    entityRelation[row[0]] = row[2]
+                    entityRelation[row[0]] = [row[2]]
                 else:
-                    entityRelation[row[0]] += row[2]
+                    entityRelation[row[0]].append(row[2])
         entityRelation.pop("Subject")
         entityList.remove("Subject")
     entity_set = set(entityList)
@@ -26,64 +27,14 @@ def getAllEntity():
 
 
 solr = pysolr.Solr('http://localhost:8983/solr/localDocs', timeout=10)
-F = 5371
 entityInfo = getAllEntity()
 objects = list(entityInfo[0])
 entityRelation = entityInfo[1]
 
 allDocs = solr.search('*:*', rows=1000000)
-# document_frequencies = {}
-# for document in allDocs:
-#     words = word_tokenize(' '.join(document['Abstract']).lower())
-#     freq_dist = FreqDist(words)
-#     for word in freq_dist.keys():
-#         document_frequencies[word] = document_frequencies.get(word, 0) + 1
-
-# step-1
-# rankObjects = {}
-
-# for object in objects:
-#     for doc in allDocs:
-#         term_fre = len(re.findall(object, ' '.join(doc['Abstract'])))
-#         if object in rankObjects.keys():
-#             if object in document_frequencies.keys():
-#                 rankObjects[object] += term_fre * \
-#                     log(F/document_frequencies[object])
-#             else:
-#                 rankObjects[object] = term_fre * \
-#                     log(F/document_frequencies[object])
-
-# step-2
-
-
-# E = len(rankObjects)
 
 document_entity_vector = {}
-for document in allDocs:
-    curr_doc_vector = {}
-    for object in objects:
-        if object in document['Abstract']:
-            if object in curr_doc_vector.keys():
-                curr_doc_vector[object] += len(re.findall(object,
-                                               document['Abstract']))
-            else:
-                curr_doc_vector[object] = len(
-                    re.findall(object, document['Abstract']))
-
-            objRelation = entityRelation[object]
-            for info in objRelation:
-                for object2 in objects:
-                    if object2 in info:
-                        if object2 in curr_doc_vector.keys():
-                            curr_doc_vector[object2] += len(
-                                re.findall(object2, info))
-                        else:
-                            curr_doc_vector[object2] = len(
-                                re.findall(object2, info))
-    document_entity_vector[document['id']] = curr_doc_vector
-
-
-with open('nfcorpus/dev.vid-desc.queries', 'r', encoding='utf-8') as file:
+with open('nfcorpus/train.titles.queries', 'r', encoding='utf-8') as file:
     for l in file:
         query_entity_freq = {}
         line = l.split('\t')
@@ -106,4 +57,37 @@ with open('nfcorpus/dev.vid-desc.queries', 'r', encoding='utf-8') as file:
                                 query_entity_freq[object2] = len(
                                     re.findall(object2, info))
 
-        for doc, doc_vector in document_entity_vector.items():
+        if query_entity_freq == {}:
+            print("No entity for :", query)
+        else:
+            score = {}
+            count = 0
+            for doc in allDocs:
+                doc_freq = {}
+                for object in objects:
+                    if object in ' '.join(doc['Title']):
+                        if object in doc_freq.keys():
+                            doc_freq[object] += len(re.findall(object,
+                                                    ' '.join(doc['Title'])))
+                        else:
+                            doc_freq[object] = len(re.findall(
+                                object, ' '.join(doc['Title'])))
+                for key in query_entity_freq.keys():
+                    if key in doc_freq.keys():
+                        if doc['id'] in score.keys():
+                            score[doc['id']] += query_entity_freq[key] * \
+                                doc_freq[key]
+                        else:
+                            score[doc['id']] = query_entity_freq[key] * \
+                                doc_freq[key]
+            sortedScore = dict(
+                sorted(score.items(), key=lambda key_val: key_val[1], reverse=True))
+            relevant_docs = list(sortedScore.keys())[:80]
+            non_relevant_docs = set("MED-" + str(i) for i in range(1, 5372))
+            for doc in relevant_docs:
+                if doc in non_relevant_docs:
+                    non_relevant_docs.remove(doc)
+            updated_query_vector = exp3Rocchio.rocchio_algorithm(
+                exp3Rocchio.term_frequency(query), relevant_docs, non_relevant_docs)
+            print(query)
+            print(updated_query_vector)
