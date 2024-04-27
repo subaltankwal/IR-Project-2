706a59e172c2ca7872a7c9dc822e8eec9e5ff9bc
Rohan121002 <f20212739@hyderabad.bits-pilani.ac.in>
2024-04-27 16:08:22 +0530

    exp2,3 ,5 added

--- exp4.py
+++ exp4.py
@@ -6,6 +6,7 @@ import math
 
 solr = pysolr.Solr('http://localhost:8983/solr/localDocs', timeout=10)
 
+
 def preprocess_text(text):
     if isinstance(text, list):
         text = ' '.join(text)
@@ -15,7 +16,6 @@ def preprocess_text(text):
     return tokens
 
 
-
 def calculate_bm25(query, doc, doc_len, avg_doc_len, doc_freq, N):
     k1 = 1.5
     b = 0.75
@@ -23,10 +23,12 @@ def calculate_bm25(query, doc, doc_len, avg_doc_len, doc_freq, N):
     score = 0
     for term in query:
         if term in doc_freq:
-            idf = math.log((N - doc_freq[term] + 0.5) / (doc_freq[term] + 0.5) + 1)
+            idf = math.log(
+                (N - doc_freq[term] + 0.5) / (doc_freq[term] + 0.5) + 1)
             tf = doc.count(term)
             score += idf * ((tf * (k1 + 1)) / (tf + K))
     return score
+
 
 def calculate_language_model(query, doc, doc_len, collection_freq, mu):
     score = 0
@@ -39,53 +41,59 @@ def calculate_language_model(query, doc, doc_len, collection_freq, mu):
                 score += p_lm
             else:
                 score *= p_lm
-                
+
     return score
 
+
 def search_with_bm25(query):
-    results = solr.search(f"Title:{query}" , rows= 200)
-    
+    results = solr.search(f"Title:{query}", rows=200)
+
     if len(results) == 0:
         return []
 
     query_tokens = preprocess_text(query)
-
     avg_doc_len = sum(len(doc['Title']) for doc in results) / len(results)
 
-    doc_freq = FreqDist([token for doc in results for token in preprocess_text(doc['Title'])])
+    doc_freq = FreqDist(
+        [token for doc in results for token in preprocess_text(doc['Title'])])
     N = len(results)
 
     ranked_results = []
     for doc in results:
         doc_text = preprocess_text(doc['Title'])
         doc_len = len(doc_text)
-        bm25_score = calculate_bm25(query_tokens, doc_text, doc_len, avg_doc_len, doc_freq, N)
+        bm25_score = calculate_bm25(
+            query_tokens, doc_text, doc_len, avg_doc_len, doc_freq, N)
         ranked_results.append((doc['id'], bm25_score))
 
     ranked_results.sort(key=lambda x: x[1], reverse=True)
 
     return ranked_results[:10]
 
+
 def search_with_language_model(query):
-    results = solr.search(f"Title:{query}" , rows= 200)
+    results = solr.search(f"Title:{query}", rows=200)
 
     query_tokens = preprocess_text(query)
 
-    collection_freq = FreqDist(token for doc in results for token in preprocess_text(doc['Title']))
+    collection_freq = FreqDist(
+        token for doc in results for token in preprocess_text(doc['Title']))
     global collection_len
     collection_len = sum(collection_freq.values())
     ranked_results = []
     for doc in results:
         doc_text = preprocess_text(doc['Title'])
         doc_len = len(doc_text)
-        lm_score = calculate_language_model(query_tokens, doc_text, doc_len, collection_freq, mu=0.7)
+        lm_score = calculate_language_model(
+            query_tokens, doc_text, doc_len, collection_freq, mu=0.7)
         ranked_results.append((doc['id'], lm_score))
 
     ranked_results.sort(key=lambda x: x[1], reverse=True)
 
     return ranked_results[:10]
 
-with open ('nfcorpus/train.titles.queries' , 'r' , encoding='utf-8') as file:
+
+with open('nfcorpus/train.titles.queries', 'r', encoding='utf-8') as file:
     for l in file:
         line = l.split('\t')
         query = line[1]
@@ -99,4 +107,5 @@ with open ('nfcorpus/train.titles.queries' , 'r' , encoding='utf-8') as file:
         lm_results = search_with_language_model(query)
         print("\nLanguage Model Results:")
         for rank, (doc_id, score) in enumerate(lm_results, start=1):
-            print(f"Rank {rank}: Document ID: {doc_id}, Language Model Score: {score}")
+            print(
+                f"Rank {rank}: Document ID: {doc_id}, Language Model Score: {score}")
--- exp6.py
+++ exp6.py
@@ -1,64 +1,51 @@
 from math import log
 import pysolr
 import csv
+from nltk import FreqDist
+from nltk import word_tokenize
 
 
 def getAllEntity():
     entityList = []
+    entityRelation = {}
     csv_file_path = "gena_data_final_triples (1).csv"
     with open(csv_file_path, 'r', encoding='utf-8', newline="") as file:
         csv_reader = csv.reader(file)
         for row in csv_reader:
             entityList.append(row[0])
+            if row[2] != "No synonyms":
+                if row[0] not in entityRelation.keys():
+                    entityRelation[row[0]] = row[2]
+                else:
+                    entityRelation[row[0]] += row[2]
+        entityRelation.pop("Subject")
         entityList.remove("Subject")
     entity_set = set(entityList)
-    return entity_set
+    return entity_set, entityRelation
 
 
 solr = pysolr.Solr('http://localhost:8983/solr/localDocs', timeout=10)
 F = 5371
-objects = list(getAllEntity())
-params = {
-    'terms': 'true',
-    'terms.fl': 'Title',
-    'terms.limit': -1,
-    'terms.sort': 'count',
-}
-df = {}
+entityInfo = getAllEntity()
+objects = list(entityInfo[0])
+entityRelation = entityInfo[1]
+
+allDocs = solr.search('*:*', rows=1000000)
+document_frequencies = {}
+for document in allDocs:
+    words = word_tokenize(' '.join(document['Abstract']).lower())
+    freq_dist = FreqDist(words)
+    for word in freq_dist.keys():
+        document_frequencies[word] = document_frequencies.get(word, 0) + 1
+
+rankObjects = {}
 for object in objects:
-    # finalObject = pysolr.escape_query_chars(object)
-    object.replace('+', r'\+').replace('(', r'\(').replace(')', r'\)')
-    response = solr.search(object, **params)
-    for res in response:
-        print(res)
-    # print(response)
-    # if 'terms' in response.raw_response:
-    #     terms = response.raw_response['terms']['Title']
-    #     for term_info in terms:
-    #         term = term_info['term']
-    #         frequency = term_info['count']
-    #         print(f'Term: {term}, Frequency: {frequency}')
-    # else:
-    #     print("No term frequency information found in the response.")
+    if object in document_frequencies.keys():
+        rankObjects[object] = document_frequencies[object] * \
+            log(F/document_frequencies[object])
 
-# print(df)
-
-# Frequency of each object in each document
-# tf = {'d1': {'o1': 5, 'o2': 7, 'o3': 10},
-#       'd2': {'o1': 8, 'o2': 6, 'o3': 12}}  # Replace with actual term frequencies
-
-
-# def rank_objects(objects, tf, df, F):
-#     rankings = {}
-#     for o in objects:
-#         # Calculate ranking score for each object
-#         score = sum(tf[d].get(o, 0) * log(F / df[o], 10) for d in tf)
-#         rankings[o] = score
-#     # Sort objects by score in descending order
-#     ranked_objects = sorted(rankings.items(), key=lambda x: x[1], reverse=True)
-#     return ranked_objects
-
-
-# Get the ranked list of objects with their scores
-# ranked_list = rank_objects(objects, tf, df, F)
-# print(ranked_list)
+E = len(rankObjects)
+with open('nfcorpus/dev.vid-desc.queries', 'r', encoding='utf-8') as file:
+    for l in file:
+        line = l.split('\t')
+        query = line[1]
